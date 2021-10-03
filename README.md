38720084906aa29da939ca966f8ffd036441867a
Aleksandr Samsonov <therion@luvu.ru>
2021-10-04 00:39:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ccp4s8yghkiltoi7?svg=true)](https://ci.appveyor.com/project/AleksandrSamsonov/aqa-lesson-3)
---
+++ src/test/java/ru/netology/cardorder/TestCardOrder.java
@@ -0,0 +1,78 @@
+package ru.netology.cardorder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestCardOrder {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSendFormWithValidData() {
+        $("[data-test-id='name'] .input__control").setValue("Сергей Иванов-Петров");
+        $("[data-test-id='phone'] .input__control").setValue("+79031112233");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSendFormWithEmptyName() {
+        $("[data-test-id='name'] .input__control").setValue("");
+        $("[data-test-id='phone'] .input__control").setValue("+79031112233");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSendFormWithNotCyrillicName() {
+        $("[data-test-id='name'] .input__control").setValue("Dmitry");
+        $("[data-test-id='phone'] .input__control").setValue("+79031112233");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSendFormWithNumbersInNameField() {
+        $("[data-test-id='name'] .input__control").setValue("123456");
+        $("[data-test-id='phone'] .input__control").setValue("+79031112233");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSendFormWithInvalidPhoneNumber() {
+        $("[data-test-id='name'] .input__control").setValue("Ирина Петрова");
+        $("[data-test-id='phone'] .input__control").setValue("+790311122334");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldSendFormWithLettersInPhoneNumberField() {
+        $("[data-test-id='name'] .input__control").setValue("Ирина Петрова");
+        $("[data-test-id='phone'] .input__control").setValue("abcde");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldSendFormWithEmptyPhoneNumber() {
+        $("[data-test-id='name'] .input__control").setValue("Юлия Нгуен");
+        $("[data-test-id='phone'] .input__control").setValue("");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+    }
+}
